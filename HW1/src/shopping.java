11d69a9f5526b97fb03378c4c4b2e6ad8927aeb8
AliaT <AliaT@192.168.1.10>
2019-02-20 21:04:43 +0200

    lateef

---
+++ HW1/src/shopping.java
@@ -0,0 +1,18 @@
+import java.util.function.BooleanSupplier;
+
+public class shopping {
+	int count=0;
+	int price=0;
+public int getcount()
+{
+	return count;
+}
+
+public void add(String string,int x) {
+	count++;
+price+=x;
+
+	return ;
+}
+
+}
---
+++ HW1/src/unit1.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class unit1 { 
+
+	@Test
+	public void test () throws Exception {
+		
+		shopping cart1=new shopping();
+		assertTrue(cart1.getcount()==0);
+	}
+@Test
+public void test2() throws Exception{
+	shopping cart2=new shopping();
+	cart2.add("Java Book",127);
+	assertTrue(cart2.count==1);
+	assertTrue(cart2.price==127);
+}
+@Test
+public void test3() throws Exception{
+	shopping cart3=new shopping();
+	cart3.add("Java Book",127);
+	cart3.add("Web Design Book",100);
+	assertTrue(cart3.count==2);
+	assertTrue(cart3.price==227);
+}
+}
